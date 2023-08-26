804dce103cf8cce95cdfd3447b40c4bf555032f6
Maxik-web <kleok0039@gmail.com>
2023-08-26 11:08:01 +0300

    first commit

--- js/main.js
+++ js/main.js
@@ -188,26 +188,3 @@ let app = new Vue({
     },
   },
 });
-let audio = new Audio("audio/soundtrack.mp3");
-let audio__btn = document.querySelector(".btn__sound");
-let audio__icon = document.querySelector(".btn__sound i");
-
-audio.muted = true;
-audio.volume = 0.2;
-
-audio.addEventListener("loadmetadata", function () {});
-
-audio__btn.addEventListener("click", function () {
-  if (audio.muted) {
-    audio.muted = false;
-    audio.currentTime = 0 + Math.random() * (audio.duration + 1 - 0);
-    audio.play()
-    audio__icon.classList.add("fa-volume-up");
-    audio__icon.classList.remove("fa-volume-off");
-  } else if (!audio.muted) {
-    audio.muted = true;
-    audio.pause()
-    audio__icon.classList.add("fa-volume-off");
-    audio__icon.classList.remove("fa-volume-up");
-  }
-});
